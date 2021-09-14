fc910d3df945c9dbb5b518f9c0c01089f17d6bcc
JavoxirNazarov <jvxrnazarov97@mail.ru>
2021-09-14 14:26:44 +0500

    android bottom sheet

--- android/app/src/main/java/com/apexorder/MainActivity.java
+++ android/app/src/main/java/com/apexorder/MainActivity.java
@@ -2,6 +2,10 @@ package com.apexorder;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -16,5 +20,16 @@ protected void onCreate(Bundle savedInstanceState) {
   @Override
   protected String getMainComponentName() {
     return "ApexOrder";
+  }
+
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+      return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
